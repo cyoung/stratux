34bc4ab0519b230dc5d3b125dfcd6aa3c3ae523d
Christopher Young <cyoung@gmail.com>
2015-09-21 21:08:08 -0400

    Prototype changes.

--- dump978/dump978.h
+++ dump978/dump978.h
@@ -4,7 +4,7 @@
 // $ gcc -c -O2 -g -Wall -Werror -Ifec -fpic -DBUILD_LIB=1 dump978.c fec.c fec/decode_rs_char.c fec/init_rs_char.c
 // $ gcc -shared -lm -o ../libdump978.so dump978.o fec.o decode_rs_char.o init_rs_char.o
 
-typedef void (*CallBack)(char updown, uint8_t *data, int len);
+typedef void (*CallBack)(char updown, uint8_t *data, int len, int rs_errors, int signal_strength);
 
 extern void Dump978Init(CallBack cb);
 extern int process_data(char *data, int dlen);
