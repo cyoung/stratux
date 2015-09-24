3e972b37535875c91f6be32dae0ff47997cc2207
Christopher Young <cyoung@gmail.com>
2015-09-24 16:31:06 -0400

    Reorg.

--- test/maxgap.go
+++ test/maxgap.go
@@ -4,7 +4,7 @@ package main
 import (
 	"fmt"
 //	"time"
-	"./uatparse"
+	"../uatparse"
 	"os"
 	"bufio"
 	"strings"
--- test/nexrad_annunciator.go
+++ test/nexrad_annunciator.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"./uatparse"
+	"../uatparse"
 	"strconv"
 	"os"
 	"bufio"
