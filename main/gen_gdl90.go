fa4718c2128af4c89602ae4d43625b47a83dd084
Christopher Young <cyoung@gmail.com>
2015-10-08 07:25:43 -0400

    Default ModeS code - not zero.

--- main/gen_gdl90.go
+++ main/gen_gdl90.go
@@ -708,6 +708,7 @@ func defaultSettings() {
 	globalSettings.AHRS_Enabled = false
 	globalSettings.DEBUG = false
 	globalSettings.ReplayLog = false //TODO: 'true' for debug builds.
+	globalSettings.OwnshipModeS = 0xF00000
 }
 
 func readSettings() {
