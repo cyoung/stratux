ec88895038b642a1a41b0e7b0e11091965fb2b25
Christopher Young <cyoung@gmail.com>
2015-11-17 16:12:41 -0500

    Logic error.

--- main/network.go
+++ main/network.go
@@ -216,7 +216,7 @@ func messageQueueSender() {
 					averageSendableQueueSize = averageSendableQueueSize / float64(len(outSockets)) // It's a total, not an average, up until this point.
 					pd = math.Max(float64(1.0/2500.0), float64(1.0/(4.0*averageSendableQueueSize))) // Say 250msg is enough to get through the whole queue.
 				} else {
-					pd = float64(1.0/2500.0)
+					pd = float64(1.0)
 				}
 				queueTimer.Stop()
 				queueTimer = time.NewTicker(time.Duration(pd * 1000000000.0) * time.Nanosecond)
