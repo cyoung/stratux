deb39cb444ddf04c0879d6d1fb10be286f0ebe8b
Christopher Young <cyoung@gmail.com>
2016-02-15 16:22:37 -0500

    Force new link for startup script.

--- selfupdate/update_footer.sh
+++ selfupdate/update_footer.sh
@@ -6,8 +6,8 @@ cp -f libimu.so /usr/lib/libimu.so
 # Startup script.
 cp -f init.d-stratux /etc/init.d/stratux
 chmod 755 /etc/init.d/stratux
-ln -s /etc/init.d/stratux /etc/rc2.d/S01stratux
-ln -s /etc/init.d/stratux /etc/rc6.d/K01stratux
+ln -fs /etc/init.d/stratux /etc/rc2.d/S01stratux
+ln -fs /etc/init.d/stratux /etc/rc6.d/K01stratux
 
 cp -f dump1090 /usr/bin/
 
