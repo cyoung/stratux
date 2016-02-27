b9f18cdc2e893171d0439871f94cd590ad1e7509
Joseph Poirier <jdpoirier@gmail.com>
2016-02-26 23:49:11 -0600

    comment update

--- main/sdr.go
+++ main/sdr.go
@@ -418,8 +418,8 @@ func configDevices(count int, es_enabled, uat_enabled bool) {
 
 // chicken and egg - to gracefully shut down a read method we
 // check a channel for a close flag, but now we want to handle
-// catastrophic dongle failures (identified when ReadSync returns
-// an error) but we can't just bypass the channel check and
+// catastrophic dongle failures (when ReadSync returns or we get
+// stderr ouput) but we can't just bypass the channel check and
 // return directly from the goroutine because the close channel
 // call in shutdown will cause a runtime panic, hence these...
 var shutdownES bool
