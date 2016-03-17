616a2b9fbd77d5954259c56000aa49fb61f62910
Christopher Young <cyoung@gmail.com>
2016-03-17 16:55:03 -0400

    Add /boot/config.txt settings.

--- image/mkimg.sh
+++ image/mkimg.sh
@@ -108,3 +108,9 @@ sed -i /etc/inittab -e "s|^.*:.*:respawn:.*ttyAMA0|#&|"
 
 #Set the keyboard layout to US.
 sed -i /etc/default/keyboard -e "/^XKBLAYOUT/s/\".*\"/\"us\"/"
+
+#boot settings
+echo "max_usb_current=1" >>mnt/boot/config.txt
+echo "dtparam=i2c1=on" >>mnt/boot/config.txt
+echo "dtparam=i2c1_baudrate=400000" >>mnt/boot/config.txt
+echo "dtparam=i2c_arm_baudrate=400000" >>mnt/boot/config.txt
