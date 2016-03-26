c6c8c22b63aa31c6d88f289e34c2fedac67a4032
Christopher Young <cyoung@gmail.com>
2016-03-26 17:23:08 -0400

    Stratux plugin beginning.

---
+++ main/plugin.go
@@ -0,0 +1,13 @@
+package StratuxPlugin
+
+import (
+	"time"
+)
+
+type StratuxPlugin struct {
+	InitFunc     func() bool
+	ShutdownFunc func() bool
+	Name         string
+	Clock        time.Time
+	Input        chan string
+}
