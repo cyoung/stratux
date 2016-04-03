22a019b9a729f120f288cd548ff6a1e8426928ce
Christopher Young <cyoung@gmail.com>
2016-04-03 14:41:06 -0400

    Remove temporary files on update.

--- selfupdate/update_header.sh
+++ selfupdate/update_header.sh
@@ -4,3 +4,4 @@ rm -rf /root/stratux-update
 mkdir -p /root/stratux-update
 cd /root/stratux-update
 mv -f /var/log/stratux.sqlite /var/log/stratux.sqlite.`date +%s`
+rm -f /var/log/stratux.sqlite-wal /var/log/stratux.sqlite-shm
