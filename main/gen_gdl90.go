21bc97e9b68feaa32dad49d30908d2c93292479a
AvSquirrel <AvSquirrel@users.noreply.github.com>
2016-04-07 08:21:15 +0000

    Add disk usage / free space to log

--- main/gen_gdl90.go
+++ main/gen_gdl90.go
@@ -29,9 +29,9 @@ import (
 	"syscall"
 	"time"
 
-	humanize "github.com/dustin/go-humanize"
-
 	"../uatparse"
+	humanize "github.com/dustin/go-humanize"
+	"github.com/ricochet2200/go-disk-usage/du"
 )
 
 // http://www.faa.gov/nextgen/programs/adsb/wsa/media/GDL90_Public_ICD_RevA.PDF
@@ -71,6 +71,8 @@ const (
 	LON_LAT_RESOLUTION = float32(180.0 / 8388608.0)
 	TRACK_RESOLUTION   = float32(360.0 / 256.0)
 )
+
+var usage *du.DiskUsage
 
 var maxSignalStrength int
 
@@ -746,6 +748,9 @@ func updateStatus() {
 	globalStatus.Uptime = int64(stratuxClock.Milliseconds)
 	globalStatus.UptimeClock = stratuxClock.Time
 	globalStatus.Clock = time.Now()
+
+	usage = du.NewDiskUsage("/")
+	globalStatus.DiskBytesFree = usage.Free()
 }
 
 type WeatherMessage struct {
@@ -974,6 +979,7 @@ type status struct {
 	HardwareBuild                              string
 	Devices                                    uint32
 	Connected_Users                            uint
+	DiskBytesFree                              uint64
 	UAT_messages_last_minute                   uint
 	UAT_products_last_minute                   map[string]uint32
 	UAT_messages_max                           uint
@@ -1091,6 +1097,7 @@ func printStats() {
 		var memstats runtime.MemStats
 		runtime.ReadMemStats(&memstats)
 		log.Printf("stats [started: %s]\n", humanize.RelTime(time.Time{}, stratuxClock.Time, "ago", "from now"))
+		log.Printf(" - Disk bytes used = %s (%.1f %%), Disk bytes free = %s (%.1f %%) \n", humanize.Bytes(usage.Used()), 100*usage.Usage(), humanize.Bytes(usage.Free()), 100*(1-usage.Usage()))
 		log.Printf(" - CPUTemp=%.02f deg C, MemStats.Alloc=%s, MemStats.Sys=%s, totalNetworkMessagesSent=%s\n", globalStatus.CPUTemp, humanize.Bytes(uint64(memstats.Alloc)), humanize.Bytes(uint64(memstats.Sys)), humanize.Comma(int64(totalNetworkMessagesSent)))
 		log.Printf(" - UAT/min %s/%s [maxSS=%.02f%%], ES/min %s/%s\n, Total traffic targets tracked=%s", humanize.Comma(int64(globalStatus.UAT_messages_last_minute)), humanize.Comma(int64(globalStatus.UAT_messages_max)), float64(maxSignalStrength)/10.0, humanize.Comma(int64(globalStatus.ES_messages_last_minute)), humanize.Comma(int64(globalStatus.ES_messages_max)), humanize.Comma(int64(len(seenTraffic))))
 		log.Printf(" - Network data messages sent: %d total, %d nonqueueable.  Network data bytes sent: %d total, %d nonqueueable.\n", globalStatus.NetworkDataMessagesSent, globalStatus.NetworkDataMessagesSentNonqueueable, globalStatus.NetworkDataBytesSent, globalStatus.NetworkDataBytesSentNonqueueable)
