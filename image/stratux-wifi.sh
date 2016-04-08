c9f9875bbc6ee0efdac634d62a582473007f8c85
peepsnet <peepsnet@yahoo.com>
2016-04-08 09:48:20 -0400

    Update to logic for narrowing down edimax nano @ wlan0
    I am now looking for the edimax nano model in LSUSB "EW-7811Un" then checking to see if it was set to wlan0 using all the MACs assigned to Edimax Corp: edimaxMac=(00:1f:1f 80:1f:02 74:da:38)

--- image/stratux-wifi.sh
+++ image/stratux-wifi.sh
@@ -5,20 +5,26 @@
 /usr/bin/killall -9 hostapd hostapd-edimax
 /usr/sbin/service isc-dhcp-server stop
 
+edimaxMac=(00:1f:1f 80:1f:02 74:da:38)
 
 #  Detect RPi2 or 3 with edimax dongle at wlan0
 #  Per http://elinux.org/RPi_HardwareHistory
 
 DAEMON_CONF=/etc/hostapd/hostapd.conf
 DAEMON_SBIN=/usr/sbin/hostapd
-EW7811Un=$(head -c 8 /sys/class/net/wlan0/address)
-#echo 'WLAN0 MAC '$EW7811Un
+EW7811Un=$(lsusb | grep EW-7811Un)
+wlan0mac=$(head -c 8 /sys/class/net/wlan0/address)
+echo 'WLAN0 MAC: '$wlan0mac
+echo 'LSUSB Returned: '$EW7811Un
 RPI_REV=`cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'`
 
-if [ "$RPI_REV" = "a01041" ] || [ "$RPI_REV" = "a21041" ]  || [ "$RPI_REV" = "900092" ]   || [ "$RPI_REV" = "a02082" ]    || [ "$RPI_REV" = "a22082" ]  &&   [ "$EW7811Un" = '74:da:38' ]; then
- # This is a RPi2B or RPi3B with Edimax Nano at wlan0
- DAEMON_CONF=/etc/hostapd/hostapd-edimax.conf
- DAEMON_SBIN=/usr/sbin/hostapd-edimax
+if [ "$RPI_REV" = "a01041" ] || [ "$RPI_REV" = "a21041" ]  || [ "$RPI_REV" = "900092" ]   || [ "$RPI_REV" = "a02082" ]    || [ "$RPI_REV" = "a22082" ]; then
+# If this is a PRPi2/3 [ "$wlan0mac" = '74:da:38' ]
+  if [ "$EW7811Un" != '' ] && [[ ${edimaxMac[*]} =~ "$wlan0mac" ]]; then
+   # If there is an Edimax Nano at wlan0
+   DAEMON_CONF=/etc/hostapd/hostapd-edimax.conf
+   DAEMON_SBIN=/usr/sbin/hostapd-edimax
+ fi
 fi
 
 #Example:
