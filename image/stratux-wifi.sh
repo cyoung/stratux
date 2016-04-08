691847cc5501ad0c12249e4b588d865215e38670
peepsnet <peepsnet@yahoo.com>
2016-04-07 23:11:33 -0400

    Update for edimax dongle in a Pi3
    Since I develop on my Stratux using a second dongle I found that if you plug in an additional wifi dongle into the RPi3 then that dongle will/might be seen as wlan0. If this dongle is an edimax wifi dongle and it is detected as wlan0 then the current file will not be able to start the hostapd service.
    
    This script will now ask the system to give the MAC address of the device at wlan0 and if the system is a RPi2 or 3.
    
    [http://macaddress.webwat.ch/vendor/Edimax_Technology_Co._Ltd.](http://macaddress.webwat.ch/vendor/Edimax_Technology_Co._Ltd.)
    
    If the system is a RPi2/3 and there is an edimax at wlan0 we will now start the hostapd-edimax driver with the hostapd-edimax.conf
    
    
    Note:
    
    As of right now there are 3 MAC blocks used by Edimax. I have 3 Edimax Nano's and all of them use the "74:da:38" Mac Block so I am not checking for the other 2 MAC blocks at this time. This could be changed in the future. 
    
    I am only guessing that all the Edimax Nano's are in the 74:da:38 range and the 2 others are other devices.

--- image/stratux-wifi.sh
+++ image/stratux-wifi.sh
@@ -11,18 +11,25 @@
 
 DAEMON_CONF=/etc/hostapd/hostapd.conf
 DAEMON_SBIN=/usr/sbin/hostapd
-EW7811Un=$(lsusb | grep EW-7811Un)
+EW7811Un=$(head -c 8 /sys/class/net/wlan0/address)
+#echo 'WLAN0 MAC '$EW7811Un
 RPI_REV=`cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'`
-if [ "$RPI_REV" = "a01041" ] || [ "$RPI_REV" = "a21041" ]  || [ "$RPI_REV" = "900092" ] && [ "$EW7811Un" != '' ]; then
- # This is a RPi2B or RPi0 with Edimax USB Wifi dongle.
+
+if [ "$RPI_REV" = "a01041" ] || [ "$RPI_REV" = "a21041" ]  || [ "$RPI_REV" = "900092" ]   || [ "$RPI_REV" = "a02082" ]    || [ "$RPI_REV" = "a22082" ]  &&   [ "$EW7811Un" = '74:da:38' ]; then
+ # This is a RPi2B.
  DAEMON_CONF=/etc/hostapd/hostapd-edimax.conf
  DAEMON_SBIN=/usr/sbin/hostapd-edimax
-else
- DAEMON_CONF=/etc/hostapd/hostapd.conf
 fi
+#if [ "$RPI_REV" = "a02082" ] || [ "$RPI_REV" = "a22082" ]; then
+ # This is a RPi3B.
+# DAEMON_CONF=/etc/hostapd/hostapd.conf
+#fi
 
-
+#Example:
+#/usr/sbin/hostapd-edimax -B /etc/hostapd/hostapd-edimax.conf
 ${DAEMON_SBIN} -B ${DAEMON_CONF}
+
+
 
 sleep 5
 
