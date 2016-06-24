629fa68a137926f678fec0a4a024a6e9c5d2142c
Christopher Young <cyoung@gmail.com>
2016-06-24 17:12:37 -0400

    Use bash to execute update script.

--- __root__stratux-pre-start.sh
+++ __root__stratux-pre-start.sh
@@ -9,7 +9,7 @@ if [ -e /root/update*stratux*v*.sh ] ; then
 		# Execute the script, remove it, then reboot.
 		echo
 		echo "Running update script ${UPDATE_SCRIPT}..."
-		sh ${UPDATE_SCRIPT}
+		bash ${UPDATE_SCRIPT}
 		rm -f $UPDATE_SCRIPT
 		reboot
 	fi
