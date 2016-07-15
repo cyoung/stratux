56874e0886944aa076da0218ab4dd10f71b2388e
AvSquirrel <AvSquirrel@users.noreply.github.com>
2016-07-15 15:50:30 +0000

    Disable console on ttyAMA0 - shell updates

--- selfupdate/update_footer.sh
+++ selfupdate/update_footer.sh
@@ -29,6 +29,10 @@ cp -f hostapd_manager.sh /usr/sbin/
 #boot config
 cp -f config.txt /boot/config.txt
 
+#disable serial console
+sed -i /etc/inittab -e "s|^.*:.*:respawn:.*ttyAMA0|#&|"
+sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+
 #modprobe.d blacklist
 cp -f rtl-sdr-blacklist.conf /etc/modprobe.d/
 
