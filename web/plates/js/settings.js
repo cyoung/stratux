7fb803f900f2227ba2ceed0ba3bc894455dd53ad
bearcode <alexbman@gmail.com>
2016-08-23 01:26:54 -0400

    remove GPX since there is no support for it yet

--- web/plates/js/settings.js
+++ web/plates/js/settings.js
@@ -7,7 +7,7 @@ function SettingsCtrl($rootScope, $scope, $state, $location, $window, $http) {
 	$scope.$parent.helppage = 'plates/settings-help.html';
 
 	var toggles = ['UAT_Enabled', 'ES_Enabled', 'Ping_Enabled', 'GPS_Enabled', 'AHRS_Enabled', 'DisplayTrafficSource', 'DEBUG', 'ReplayLog', 'GPS_Track_Enabled'];
-	$scope.format_options = ['KML', 'GPX'];
+	$scope.format_options = ['KML'];
 	var settings = {};
 	for (i = 0; i < toggles.length; i++) {
 		settings[toggles[i]] = undefined;
