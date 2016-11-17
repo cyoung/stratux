2b3d06eafa881a734f198402c79dfcf0e7154247
Jim Jacobsen <jamez1970@gmail.com>
2016-11-17 07:25:36 +0000

    Fixed JSON call

--- main/network.go
+++ main/network.go
@@ -143,7 +143,7 @@ func sendToAllConnectedClients(msg networkMessage) {
 	if (msg.msgType & NETWORK_GDL90_STANDARD) != 0 {
 		// It's a GDL90 message. Send to serial output channel (which may or may not cause something to happen).
 		serialOutputChan <- msg.msg
-                networkGDL90Chan <- msg.msg
+		networkGDL90Chan <- msg.msg
 	}
 
 	netMutex.Lock()
@@ -196,14 +196,15 @@ var serialOutputChan chan []byte
 var networkGDL90Chan chan []byte
 
 func networkOutWatcher() {
-    //ticker := time.NewTicker(10 * time.Second)
-    //var nmsg gdl90NetMessage
-    for {
-        select {
-            case ch := <-networkGDL90Chan:
-            	gdl90Update.SendJSON(ch)
-        }
-    }
+	//ticker := time.NewTicker(10 * time.Second)
+	//var nmsg gdl90NetMessage
+	for {
+		select {
+		case ch := <-networkGDL90Chan:
+			gdlJSON, _ := json.Marshal(ch)
+			gdl90Update.Send(gdlJSON)
+		}
+	}
 }
 
 // Monitor serial output channel, send to serial port.
@@ -616,7 +617,7 @@ func ffMonitor() {
 func initNetwork() {
 	messageQueue = make(chan networkMessage, 1024) // Buffered channel, 1024 messages.
 	serialOutputChan = make(chan []byte, 1024)     // Buffered channel, 1024 GDL90 messages.
-        networkGDL90Chan = make(chan []byte, 1024)
+	networkGDL90Chan = make(chan []byte, 1024)
 	outSockets = make(map[string]networkConnection)
 	pingResponse = make(map[string]time.Time)
 	netMutex = &sync.Mutex{}
