8efa923a8c9751d1ad1ee66674d84ce7024dd92f
Thorsten Biermann <thorsten.biermann@gmail.com>
2017-07-06 20:51:26 +0200

    Adds X-Plane message examples and comments

--- main/xplane.go
+++ main/xplane.go
@@ -16,11 +16,14 @@ func convertFeetToMeters(feet float32) float32 {
 }
 
 func createXPlaneGpsMsg(latDeg float32, lonDeg float32, altMslFt float32) []byte {
+	// example: XGPS1,-122.298432,47.450756,420.9961,349.7547,57.9145
 	// TODO find out what the remaining parameters are for
+	// could be: track (in degrees), speed (in ?)
 	return []byte(fmt.Sprintf("XGPS1,%.6f,%.6f,%.4f,%.4f,%.4f", lonDeg, latDeg, convertFeetToMeters(altMslFt), 0.0, 0.0))
 }
 
 func createXPlaneAttitudeMsg(headingDeg float32, pitchDeg float32, rollDeg float32) []byte {
+	// example: XATT1,345.1,-1.1,-12.5,0.1374,0.0954,-0.0444,-17.0,-1.2,-65.0,-0.01,1.63,0.02
 	// TODO find out what the remaining parameters are for
 	return []byte(fmt.Sprintf("XATT1,%.1f,%.1f,%.1f,%.4f,%.4f,%.4f,%.1f,%.1f,%.1f,%.2f,%.2f,%.2f", headingDeg, pitchDeg, rollDeg, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
 }
