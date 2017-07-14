5c1e52905dae7d500664d77df95ce918f1f310b1
peepsnet <peepsnet@yahoo.com>
2017-07-14 14:45:17 -0400

    Update __root__stratux-pre-start.sh

--- __root__stratux-pre-start.sh
+++ __root__stratux-pre-start.sh
@@ -26,7 +26,6 @@ fi
 
 # Check if we need to run an update.
 if [ -e ${UPDATE_LOCATION} ]; then
-	echo " ls -1t ${UPDATE_LOCATION} | head -1"  >> $STX_LOG
 	UPDATE_SCRIPT=`ls -1t ${UPDATE_LOCATION} | head -1`
 	if [ -n ${UPDATE_SCRIPT} ] ; then
 		# Execute the script, remove it, then reboot.
