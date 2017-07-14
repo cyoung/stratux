45a213a35279b53f43aac45d777f2d5e27024905
peepsnet <peepsnet@yahoo.com>
2017-07-14 13:55:03 -0400

    Allow updates from /boot/StratuxUpdates
    I have often flown with a backup SD card. This SD card is a "virgin" card that has never been in my stratux before. I just write the latest IMG. 
    
    I have always wanted to be able to have stratux update to the latest updates.sh files because there are features in the updates that are proven, and sometimes needed.  This would not be possible in flight with no internet and too distracting to update from my tablet in flight. 
    
    My Idea was to allow update files to be placed on the /boot partition since it is visible to Windows. 
    
    If you place an update, like update-stratux-v1.3b3-1877e66056.sh from http://updates.stratux.me/builds/, in a folder in /boot called StratuxUpdates(/boot/StratuxUpdates) it will automatically update that SH

--- __root__stratux-pre-start.sh
+++ __root__stratux-pre-start.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-
+echo "Running"
 echo powersave >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
+SCRIPT_MASK="update*stratux*v*.sh"
+TEMP_LOCATION="/boot/StratuxUpdates/$SCRIPT_MASK"
+UPDATE_LOCATION="/root/$SCRIPT_MASK"
+
+echo "Temp Location $TEMP_LOCATION"
+
+if [ -e ${TEMP_LOCATION} ]; then
+	echo "Found Update Script in $TEMP_LOCATION$SCRIPT_MASK"
+	TEMP_SCRIPT=`ls -1t ${TEMP_LOCATION} | head -1`
+	echo "Moving Script $TEMP_SCRIPT"
+	cp -r $TEMP_SCRIPT /root/
+	chmod a+x $UPDATE_LOCATION
+	rm -rf $TEMP_SCRIPT
+fi
+
 # Check if we need to run an update.
-if [ -e /root/update*stratux*v*.sh ] ; then
-	UPDATE_SCRIPT=`ls -1t /root/update*stratux*v*.sh | head -1`
-	if [ -n "$UPDATE_SCRIPT" ] ; then
+if [ -e ${UPDATE_LOCATION} ]; then
+	echo "ls -1t ${UPDATE_LOCATION} | head -1"
+	UPDATE_SCRIPT=`ls -1t ${UPDATE_LOCATION} | head -1`
+	if [ -n ${UPDATE_SCRIPT} ] ; then
 		# Execute the script, remove it, then reboot.
 		echo
 		echo "Running update script ${UPDATE_SCRIPT}..."
@@ -14,4 +30,3 @@ if [ -e /root/update*stratux*v*.sh ] ; then
 		reboot
 	fi
 fi
-
