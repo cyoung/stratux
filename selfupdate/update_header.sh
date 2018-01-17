1a1cf2e93c902870a195f5450016db09db1b11cd
cyoung <cyoung@gmail.com>
2018-01-17 10:33:13 -0500

    Delete /var/log/stratux* on update.

--- selfupdate/update_header.sh
+++ selfupdate/update_header.sh
@@ -3,5 +3,4 @@
 rm -rf /root/stratux-update
 mkdir -p /root/stratux-update
 cd /root/stratux-update
-rm -f /var/log/stratux.sqlite /var/log/stratux.sqlite-wal /var/log/stratux.sqlite-shm
-rm -f /var/log/stratux.log
+rm -f /var/log/stratux*
