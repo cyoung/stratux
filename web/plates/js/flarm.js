0b7c11d5618056d6359f05482e353cef1914456a
root <dev@batzill.com>
2019-05-08 09:32:16 +0200

    made flarm status page actually work in practice

--- web/plates/js/flarm.js
+++ web/plates/js/flarm.js
@@ -1,5 +1,7 @@
 angular.module('appControllers').controller('FlarmCtrl', FlarmCtrl); // get the main module contollers set
-FlarmCtrl.$inject = ['$rootScope', '$scope', '$state', '$http', '$interval']; // Inject my dependencies
+FlarmCtrl.$inject = ['$rootScope', '$scope', '$state', '$http', '$interval', '$sce']; // Inject my dependencies
 
-function FlarmCtrl($rootScope, $scope, $state, $http, $interval) {
+function FlarmCtrl($rootScope, $scope, $state, $http, $interval, $sce) {
+    $scope.ogn_rf_url = $sce.trustAsResourceUrl('http://' + window.location.hostname + ':8082');
+    $scope.ogn_decode_url = $sce.trustAsResourceUrl('http://' + window.location.hostname + ':8083');
 }
