c43b3b086f4f060fb48b13c434fd067bfd13ac36
Adrian Batzill <dev@batzill.com>
2021-05-15 21:39:14 +0200

    update flashing script to new tracker firmware filename

--- ogn/install-ogntracker-firmware-pi.sh
+++ ogn/install-ogntracker-firmware-pi.sh
@@ -14,7 +14,7 @@ cd /tmp
 
 python esptool.py --chip esp32 --port /dev/serialin --baud 921600 --before default_reset --after hard_reset \
     write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 \
-    build/bootloader/bootloader.bin 0x10000 build/app-template.bin 0x8000 build/partitions.bin
+    build/bootloader/bootloader.bin 0x10000 build/esp32-ogn-tracker.bin 0x8000 build/partitions.bin
 
 systemctl start stratux
 
