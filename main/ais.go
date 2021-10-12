e80c66025355a5c8647c350dc181592f4b7c46ca
rvt <github@rvt.dds.nl>
2021-10-12 22:38:17 +0200

    Work on sending AIS message in a format that Stratux can understand

--- main/ais.go
+++ main/ais.go
@@ -11,49 +11,20 @@ package main
 
 import (
 	"bufio"
-	"encoding/binary"
-	"encoding/hex"
 	"encoding/json"
 //	"io/ioutil"
 	"log"
 	"net"
-	"strconv"
 	"strings"
 	"time"
+	"fmt"
 
 	"github.com/b3nn0/stratux/common"
-	ais "github.com/BertoldVdb/go-ais"
+
+	"github.com/BertoldVdb/go-ais"
 	"github.com/BertoldVdb/go-ais/aisnmea"
 )
 
-
-// {"sys":"OGN","addr":"395F39","addr_type":3,"acft_type":"1","lat_deg":51.7657533,"lon_deg":-1.1918533,"alt_msl_m":124,"alt_std_m":63,"track_deg":0.0,"speed_mps":0.3,"climb_mps":-0.5,"turn_dps":0.0,"DOP":1.5}
-type AISMessage struct {
-	Sys string
-	Time int64
-	Addr string
-	Addr_type int32
-	Acft_type string
-	Acft_cat string
-	Reg string
-	Lat_deg float32
-	Lon_deg float32
-	Alt_msl_m float32
-	Alt_hae_m float32
-	Alt_std_m float32
-	Track_deg float64
-	Speed_mps float64
-	Climb_mps float64
-	Turn_dps float64
-	DOP float64
-	SNR_dB float64
-	Rx_err int32
-
-	// Status message (Sys=status):
-	Bkg_noise_db float32
-	Gain_db      float32
-	Tx_enabled   bool
-}
 
 func aisPublishNmea(nmea string) {
 	if globalStatus.AIS_connected {
@@ -67,7 +38,7 @@ func aisPublishNmea(nmea string) {
 var aisOutgoingMsgChan chan string = make(chan string, 100)
 var aisIncomingMsgChan chan string = make(chan string, 100)
 var aisExitChan chan bool = make(chan bool, 1)
-var aisTailNumberCache = make(map[string]string)
+var aisTailNumberCache = make(map[uint32]string)
 
 func aisListen() {
 	//go predTest()
@@ -115,22 +86,8 @@ func aisListen() {
 				aisReadWriter.Write([]byte(data))
 				aisReadWriter.Flush()
 			case data := <- aisIncomingMsgChan:
-				log.Printf("AIS: " + data)
-				result, _ := nm.ParseSentence(data)
-				out, _ := json.MarshalIndent(result, "", "  ")
-				log.Printf("AIS: " + string(out))
-//				var thisMsg msg
-//				thisMsg.MessageClass = MSGCLASS_AIS
-//				thisMsg.TimeReceived = stratuxClock.Time
-//				thisMsg.Data = data
-	
-//				var msg AISMessage
-//				err = json.Unmarshal([]byte(data), &msg)
-//				if err != nil {
-//					log.Printf("Invalid Data from AIS: " + data)
-//					continue
-//				}
-	
+//				log.Printf("AIS: " + data)
+				msg, _ := nm.ParseSentence(data)
 //				if msg.Sys == "status" {
 //					importAISStatusMessage(msg)
 //				} else {
@@ -138,8 +95,12 @@ func aisListen() {
 					//msgLogAppend(thisMsg)
 					// TODO: RVT Renable
 					//logMsg(thisMsg) // writes to replay logs
-//					importAISTrafficMessage(msg, data)
 //				}
+				if (msg!=nil) {
+					importAISTrafficMessage(msg)
+				} else {
+					log.Printf("Invalid Data from AIS: " + data)
+				}
 			case <- aisExitChan:
 				break loop
 
@@ -158,160 +119,133 @@ func aisListen() {
 // 	}
 // }
 
-func importAISTrafficMessage(msg AISMessage, data string) {
+func importAISTrafficMessage(msg *aisnmea.VdmPacket) {
 	var ti TrafficInfo
-	addressBytes, _ := hex.DecodeString(msg.Addr)
-	addressBytes = append([]byte{0}, addressBytes...)
-	address := binary.BigEndian.Uint32(addressBytes)
-
-	// GDL90 only knows 2 address types. ICAO and non-ICAO, so we map to those.
-	// for AIS: 1=ICAO. For us: 0=ICAO, 1="ADS-B with Self-assigned address"
-	addrType := uint8(1) // Non-ICAO Address
-	otherAddrType := uint8(0)
-	if msg.Addr_type == 1 { // ICAO Address
-		addrType = 0 
-		otherAddrType = 1;
-	}
-	// Store in higher-order bytes in front of the 24 bit address so we can handle address collinsions gracefully.
-	// For ICAO it will be 0, so traffic is merged. For others it will be 1, so traffic is kept seperately
-	// Only issue: PAW and FANET don't know the concept of address types. So for those, we need to be more tolerant.
-	// There are 2 cases here:
-	// - If non-PAW/FNT is received first, we can simply merge PAW/FNT onto that
-	// - If PAW/FNT is received first, we might need to update the traffic type later on
-	// To make the code a bit simpler, we don't actually update the existing traffic in the second case, but just let it time out
-	// and - from then on - only update the one with the correct AddrType
-
-	key := uint32(addrType) << 24 | address
-	otherKey := uint32(otherAddrType) << 24 | address
-
-	trafficMutex.Lock()
-	defer trafficMutex.Unlock()
-
-	if msg.Sys == "PAW" || msg.Sys == "FNT" {
-		// First, assume the AddrType guess is wrong and try to merge.. Only if that fails we use our guessed AddrType
-		_, otherAddrTypeOk := traffic[otherKey]
-		if otherAddrTypeOk {
-			key = otherKey
-			addrType = otherAddrType
-		}
-	}
+	var isPositionReport = false
+	
+	var header *ais.Header = msg.Packet.GetHeader()
+	var key = header.UserID
 
 	if existingTi, ok := traffic[key]; ok {
 		ti = existingTi
-		// ais-rx sends 2 types of messages.. normal ones with coords etc, and ones that only supply registration info. These usually don't have
-		// coords, so we can't validate them easily. Therefore, this is handled before other validations and only if we already received the traffic earlier
-		if len(msg.Reg) > 0 {
-			ti.Tail = msg.Reg
-			traffic[key] = ti
+	} else {
+		ti.Tail = fmt.Sprintf("%d", header.UserID)
+	}
+	
+	trafficMutex.Lock()
+	defer trafficMutex.Unlock()
+
+	ti.Icao_addr = header.UserID
+
+	// Binary Broadcast, we will ignore this
+	// !AIVDM,3,1,8,A,8h30otA?0@<o;NPPPP<i>nskl4tSp1m>@00o;NPPPP<iCnsm<4tPG286@00o;NPP,0*3D
+	// !AIVDM,3,2,8,A,PP<j>nsphTtHBR7@@00o;NPPPP<jCnssG4tCk2N0@00o;NPPPP<k>nsuPTt6m2Mt,0*44
+    // !AIVDM,3,3,8,A,@00o;NPPPP<kCnsuwTt3?2lB@00,2*3D
+
+	// Handle Ship Static Data
+	if header.MessageID == 5 {
+		// !AIVDM,2,1,5,B,A02VqLPA4I6C07h5G`Qh>?k52kdgwT42>kOtfhls9gqW13daw?;w>jStig4s,0*78
+		// !AIVDM,2,2,5,B,<?me6SdWwml0>rbb,0*47
+		var shipStaticData ais.ShipStaticData = msg.Packet.(ais.ShipStaticData);
+		ti.Tail = shipStaticData.Name
+	}
+
+	// TODO: RVT further implement LongRangeAisBroadcastMessage ??
+	if header.MessageID==27 {
+		// var positionReport ais.LongRangeAisBroadcastMessage = msg.Packet.(ais.LongRangeAisBroadcastMessage);
+	}
+
+	// Handle MessageID 1,2 & 3 Position reports
+	if header.MessageID==1 || header.MessageID==2 || header.MessageID==3 {
+		// !AIVDM,1,1,,A,13aIhV?P140H?T@MVbNJVOvT00Ss,0*6D
+		isPositionReport = true
+		var positionReport ais.PositionReport = msg.Packet.(ais.PositionReport);
+	
+		ti.Reg = ""
+	//	ti.Tail // Set above
+		ti.Emitter_category = 0
+		ti.OnGround = true
+		ti.Addr_type = uint8(1) // Non-ICAO Address
+		ti.TargetType = uint8(1) // Non-ICAO Address
+		ti.SignalLevel = 0.0
+	//    ti.SignalLevelHist
+		ti.Squawk = 0
+		ti.Position_valid = true
+		ti.Lat = float32(positionReport.Latitude)
+		ti.Lng = float32(positionReport.Longitude)
+		ti.Alt = 0 // pressure altitude
+	//	ti.GnssDiffFromBaroAlt = 0
+	//	ti.AltIsGNSS = 0
+	//	ti.NIC = 0
+	//	ti.NACp = 0
+		ti.Track = float32(positionReport.TrueHeading)
+		ti.TurnRate = float32(positionReport.RateOfTurn)
+		ti.Speed = uint16(positionReport.Sog)
+		ti.Speed_valid = true
+	//	ti.Vvel = 0 
+		ti.Timestamp = time.Now().UTC()	
+	//	ti.PriorityStatus
+		ti.Age = time.Now().UTC().Sub(ti.Timestamp).Seconds()
+	//	ti.AgeLastAlt           float64   // Age of last altitude message, seconds ago.
+		ti.Last_seen = stratuxClock.Time
+		ageMs := int64(ti.Age * 1000)
+		ti.Last_seen = ti.Last_seen.Add(-time.Duration(ageMs) * time.Millisecond)
+		ti.Last_alt = ti.Last_seen
+		ti.Last_speed = ti.Last_seen
+	
+	//	Last_GnssDiff        
+	//	Last_GnssDiffAlt     
+		ti.Last_source = TRAFFIC_SOURCE_AIS
+		ti.ExtrapolatedPosition = false
+	//	ti.Last_extrapolation   time.Time
+	//	AgeExtrapolation     float64
+	//	Lat_fix              float32   // Last real, non-extrapolated latitude
+	//	Lng_fix              float32   // Last real, non-extrapolated longitude
+	//	Alt_fix              int32     // Last real, non-extrapolated altitude		return
+	}
+
+	// Validate the position report 
+	if isPositionReport {
+		if isGPSValid() {
+			ti.Distance, ti.Bearing = common.Distance(float64(mySituation.GPSLatitude), float64(mySituation.GPSLongitude), float64(ti.Lat), float64(ti.Lng))
+			ti.BearingDist_valid = true
 		}
-		if msg.Time > 0 && !ti.Timestamp.IsZero() {
- 			msgtime := time.Unix(msg.Time, 0)
-			if ti.Position_valid && ti.Last_source == TRAFFIC_SOURCE_AIS && msgtime.Before(ti.Timestamp) {
-				return // We already have a newer message for this target. This message was probably relayed by another tracker -- skip
-			}
+	
+	//	DistanceEstimated    float64   // Estimated distance of the target if real distance can't be calculated, Estimated from signal strength with exponential smoothing.
+	//	DistanceEstimatedLastTs time.Time // Used to compute moving average
+	//	ReceivedMsgs         uint64    // Number of messages received by this aircraft
+	
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// Validations and checks                                                                                     //
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+		// Sometimes there seems to be wildly invalid lat/lons, which can trip over distRect's normailization..
+		if ti.Lat > 360 || ti.Lat < -360 || ti.Lng > 360 || ti.Lng < -360 {
+		//	return
 		}
-
-	}
-
-	// Sometimes there seems to be wildly invalid lat/lons, which can trip over distRect's normailization..
-	if msg.Lat_deg > 360 || msg.Lat_deg < -360 || msg.Lon_deg > 360 || msg.Lon_deg < -360 {
-		return
-	}
-
-	// Basic plausibility check:
-	dist, _, _, _ := common.DistRect(float64(mySituation.GPSLatitude), float64(mySituation.GPSLongitude), float64(msg.Lat_deg), float64(msg.Lon_deg))
-	if (isGPSValid() && dist >= 50000)  || (msg.Lat_deg == 0 && msg.Lon_deg == 0) {
-		// more than 50km away? Ignore. Most likely invalid data
-		return
-	}
-
-	ti.Icao_addr = address
-	ti.Addr_type = addrType
-
-	if len(ti.Tail) == 0 {
-		ti.Tail = getTailNumber(msg.Addr, msg.Sys)
-	}
-	ti.Last_source = TRAFFIC_SOURCE_AIS
-	if msg.Time > 0 {
-		ti.Timestamp = time.Unix(msg.Time, 0)
-	} else {
-		ti.Timestamp = time.Now().UTC()
-	}
-	ti.Age = time.Now().UTC().Sub(ti.Timestamp).Seconds()
-	if ti.Age > 30 || ti.Age < -2 {
-		log.Printf("Discarding likely invalid AIS target: %s", data)
-		return
-	}
-
-	// set altitude
-	// To keep the rest of the system as simple as possible, we want to work with barometric altitude everywhere.
-	// To do so, we use our own known geoid separation and pressure difference to compute the expected barometric altitude of the traffic.
-	// Some AIS trackers are equiped with a baro sensor, but older firmwares send wrong data, so we usually can't rely on it.
-	alt := msg.Alt_msl_m * 3.28084
-	if alt == 0 {
-		alt = msg.Alt_hae_m * 3.28084 - mySituation.GPSGeoidSep
-	}
-	if isGPSValid() && isTempPressValid() {
-		ti.Alt = int32(alt - mySituation.GPSAltitudeMSL + mySituation.BaroPressureAltitude)
-		ti.AltIsGNSS = false
-	} else if msg.Alt_std_m != 0 {
-		// Fall back to received baro alt
-		ti.Alt = int32(msg.Alt_std_m * 3.28084)
-		ti.AltIsGNSS = false
-	} else {
-		// Fall back to GNSS alt
-		ti.Alt = int32(alt)
-		ti.AltIsGNSS = true
-	}
-
-	// Maybe the sender has baro AND GNS altitude.. in that case we can use that to estimage GnssBaroDiff to guess our own baro altitude
-	// TODO: don't do that because of invalid baro alts from old AIS trackers.
-	/*if msg.Alt_msl_m != 0 && msg.Alt_std_m != 0 {
-		ti.Last_GnssDiffAlt = ti.Alt
-		hae := msg.Alt_msl_m + mySituation.GPSGeoidSep
-		ti.GnssDiffFromBaroAlt = int32((hae - msg.Alt_std_m) * 3.28084)
-		ti.Last_GnssDiff = stratuxClock.Time
-	} else if msg.Alt_hae_m != 0 && msg.Alt_std_m != 0 {
-		ti.Last_GnssDiffAlt = ti.Alt
-		ti.GnssDiffFromBaroAlt = int32((msg.Alt_hae_m - msg.Alt_std_m) * 3.28084)
-		ti.Last_GnssDiff = stratuxClock.Time
-	}*/
-
-	ti.TurnRate = float32(msg.Turn_dps)
-	if ti.TurnRate > 360 || ti.TurnRate < -360 {
-		ti.TurnRate = 0
-	}
-	ti.Vvel = int16(msg.Climb_mps * 196.85)
-	ti.Lat = msg.Lat_deg
-	ti.Lng = msg.Lon_deg
-	ti.Track = float32(msg.Track_deg)
-	ti.Speed = uint16(msg.Speed_mps * 1.94384)
-	ti.Speed_valid = true
-	ti.SignalLevel = msg.SNR_dB
-
-	if isGPSValid() {
-		ti.Distance, ti.Bearing = common.Distance(float64(mySituation.GPSLatitude), float64(mySituation.GPSLongitude), float64(ti.Lat), float64(ti.Lng))
-		ti.BearingDist_valid = true
-	}
-	ti.Position_valid = true
-	ti.ExtrapolatedPosition = false
-	ti.Last_seen = stratuxClock.Time
-	ageMs := int64(ti.Age * 1000)
-	ti.Last_seen = ti.Last_seen.Add(-time.Duration(ageMs) * time.Millisecond)
-	ti.Last_alt = ti.Last_seen
-	ti.Last_speed = ti.Last_seen
-
-	emitter, err := strconv.ParseInt(msg.Acft_cat, 16, 8)
-	if len(msg.Acft_cat) == 2 && err == nil {
-		ti.Emitter_category = uint8(emitter)
-	} else {
-		ti.Emitter_category = nmeaAircraftTypeToGdl90(msg.Acft_type)
+	
+		if ti.TurnRate > 360 || ti.TurnRate < -360 {
+			ti.TurnRate = 0
+		}
+	
+		// Basic plausibility check:
+		dist, _, _, _ := common.DistRect(float64(mySituation.GPSLatitude), float64(mySituation.GPSLongitude), float64(ti.Lat), float64(ti.Lng))
+		if (isGPSValid() && dist >= 100000) {
+			// more than 100km away? Ignore. Most likely invalid data
+		//	return
+		}
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 	}
 
 	traffic[key] = ti
+
 	// postProcessTraffic(&ti) For AIS we do need to post process traffic, we always get positions
-	registerTrafficUpdate(ti)
-	seenTraffic[key] = true
+	if (isPositionReport) {
+		registerTrafficUpdate(ti)
+		seenTraffic[key] = true	
+	}
 
 	if globalSettings.DEBUG {
 		txt, _ := json.Marshal(ti)
