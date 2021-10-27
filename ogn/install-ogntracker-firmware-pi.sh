b61059bf6694c7bcd242183193e9704f01a44c60
Adrian Batzill <dev@batzill.com>
2021-10-27 18:23:01 +0200

    bullseye testing

--- ogn/install-ogntracker-firmware-pi.sh
+++ ogn/install-ogntracker-firmware-pi.sh
@@ -12,7 +12,7 @@ unzip $fwfile -d /tmp/
 systemctl stop stratux
 cd /tmp
 
-python esptool.py --chip esp32 --port /dev/serialin --baud 921600 --before default_reset --after hard_reset \
+python3 esptool.py --chip esp32 --port /dev/serialin --baud 921600 --before default_reset --after hard_reset \
     write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 \
     build/bootloader/bootloader.bin 0x10000 build/esp32-ogn-tracker.bin 0x8000 build/partitions.bin
 
