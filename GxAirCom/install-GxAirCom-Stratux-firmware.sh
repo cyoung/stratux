e4bd4cba0e3ba6a2168089b6707a8cdf65ad7eac
Adrian Batzill <dev@batzill.com>
2023-07-09 10:14:52 +0100

    erase_flash before writing GxAirCom firmware

--- GxAirCom/install-GxAirCom-Stratux-firmware.sh
+++ GxAirCom/install-GxAirCom-Stratux-firmware.sh
@@ -42,6 +42,8 @@ echo ""
 sudo systemctl stop stratux
 echo ""
 
+python3 esptool.py --chip esp32 --port $usbDevice --baud 921600 --before default_reset erase_flash
+
 python3 esptool.py --chip esp32 --port $usbDevice --baud 921600 --before default_reset --after hard_reset \
     write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect \
     0x1000 bootloader_dio_40m.bin 0x10000 $fwName.bin 0x8000 partitions.bin 0x3d0000 spiffs.bin 0xe000 boot_app0.bin
