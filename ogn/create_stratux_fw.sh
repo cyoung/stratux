f08079c808e8afa7e5b6a9fddd4979bf6bfef31f
Adrian Batzill <dev@batzill.com>
2024-06-17 15:46:19 +0200

    migrate to new ogn-tracker project

--- ogn/create_stratux_fw.sh
+++ ogn/create_stratux_fw.sh
@@ -1,26 +1,114 @@
 #!/bin/bash
+# Requires having platformio installed in your $PATH!
+
 set -e
 
-cd "$(dirname "$0")"
-
-if [[ ! -d esp-idf ]]; then
-    echo
-    read -p "Install esp-idf? [y/n]" -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        git clone -b v4.4.4 --recursive https://github.com/espressif/esp-idf.git
-        cd esp-idf && ./install.sh
-        cd ..
-    else
-        echo "esp-idf required to build firmware"
-        exit 1
-    fi
+if ! command -v platformio; then
+    echo "Please install platformio to your PATH first"
+    exit 1
 fi
 
-source esp-idf/export.sh
-cd esp32-ogn-tracker
-cd utils && make read_log && make serial_dump && cd ..
+cd "$(dirname "$0")"
+STRATUX_OGN_DIR="$(pwd)"
 
+curl https://raw.githubusercontent.com/pjalocha/esp32-ogn-tracker/master/esptool.py > esptool.py
+
+cd ogn-tracker
+
+
+function append_defaultconfig {
+    echo \
+'   -DTTGO_TBEAM
+    -DWITH_ESP32
+    -DWITH_OGN
+    -DWITH_ADSL
+    -DWITH_FANET
+    -DWITH_CONFIG     ; allow to change parameters via serial console
+    -DWITH_GPS_NMEA_PASS
+    -DWITH_OLED
+    -DWITH_GPS_PPS    ; use the PPS of the GPS (not critical but gets betterr timing)
+    ;-DWITH_GPS_CONFIG ; GPS can be adjusted for serial baud rate and navigation model
+    -DWITH_BME280     ; recognizes automatically BMP280 or BME280
+    ;-DWITH_BT_SPP     ; BT Standard Serial Port => connection to XCsoar, takes big resources in Flash and RAM
+    -DWITH_LOOKOUT
+    -DWITH_PFLAA
+    -DRADIOLIB_GODMODE' >> platformio.ini
+}
+
+function package {
+    fname=$1
+    cd .pio/build/$fname
+    cp $STRATUX_OGN_DIR/esptool.py .
+    zip $fname.zip bootloader.bin firmware.bin partitions.bin esptool.py
+    mv $fname.zip $STRATUX_OGN_DIR/
+    cd -
+}
+
+
+function create_fw {
+    name=$1
+    board=$2
+    flags=$3
+
+    git checkout platformio.ini
+    echo "
+[env:$name]
+board = $board
+build_flags = $flags" >> platformio.ini
+    append_defaultconfig
+    platformio run -e $name
+    package $name
+
+    git checkout platformio.ini
+}
+
+
+# Build SX1276-tbeam-07
+create_fw ogn-tracker-bin-tbeam07-sx1276 ttgo-lora32-v1 '
+    -DWITH_TBEAM07
+    -DWITH_SX1276
+    -DWITH_GPS_UBX
+    -DWITH_GPS_UBX_PASS'
+
+# Build SX1276-tbeam-10
+create_fw ogn-tracker-bin-tbeam10-sx1276 ttgo-lora32-v1 '
+    -DWITH_TBEAM10
+    -DWITH_SX1276
+    -DWITH_GPS_UBX
+    -DWITH_GPS_UBX_PASS'
+
+
+# Build SX1276-tbeam-20
+create_fw ogn-tracker-bin-tbeam20-sx1276 ttgo-lora32-v1 '
+    -DWITH_TBEAM20
+    -DWITH_SX1276
+    -DWITH_GPS_UBX
+    -DWITH_GPS_UBX_PASS'
+
+# Build SX1262-tbeam-10
+create_fw ogn-tracker-bin-tbeam10-sx1262 ttgo-lora32-v1 '
+    -DWITH_TBEAM10
+    -DWITH_SX1262
+    -DWITH_GPS_UBX
+    -DWITH_GPS_UBX_PASS'
+
+# Build SX1262-tbeam-s3-mtk
+#create_fw ogn-tracker-bin-tbeams3-sx1262-mtk esp32-s3-devkitc-1 '
+#    -DWITH_TBEAMS3
+#    -DWITH_SX1262
+#    -DWITH_GPS_MTK
+#    -DWITH_GPS_ENABLE'
+
+# Build SX1262-tbeam-s3-ubx
+#create_fw ogn-tracker-bin-tbeams3-sx1262-ubx esp32-s3-devkitc-1 '
+#    -DWITH_TBEAMS3
+#    -DWITH_SX1262
+#    -DWITH_GPS_UBX
+#    -DWITH_GPS_UBX_PASS'
+
+exit 0
+
+### TODO: This was our legacy build for old esp32-ogn-tracker. Keeping this here to show which options we used in the past
 
 function disable {
     opt=$1
--- ogn/install-ogntracker-firmware-pi.sh
+++ ogn/install-ogntracker-firmware-pi.sh
@@ -3,12 +3,11 @@
 
 cd "$(dirname "$0")"
 
-fwfile="esp32-ogn-tracker-bin-10+.zip"
-if [ "$1" == "07" ] || [ "$1" == "0.7" ]; then
-    fwfile="esp32-ogn-tracker-bin-07.zip"
-fi
-if [ "$1" == "sx1262" ]; then
-    fwfile="esp32-ogn-tracker-bin-10+-sx1262.zip"
+fwfile="$1.zip"
+echo $fwfile
+if [ ! -f $fwfile ]; then
+    echo "Unknown firmware $1"
+    exit 1
 fi
 
 unzip $fwfile -d /tmp/
@@ -17,7 +16,7 @@ cd /tmp
 
 python3 esptool.py --chip esp32 --port /dev/serialin --baud 921600 --before default_reset --after hard_reset \
     write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 \
-    build/bootloader/bootloader.bin 0x10000 build/esp32-ogn-tracker.bin 0x8000 build/partitions.bin
+    bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
 
 systemctl start stratux
 
